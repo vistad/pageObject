9abbb73a89c214890b2638a4329ea4c75845467b
Victor Stadnichenko <vstadnichenko@gmail.com>
2022-08-17 19:32:00 +0300

    improved error messages

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,15 +1,25 @@
-# pytest -v -s --language=en base_page.py
+from selenium.common.exceptions import NoSuchElementException
 
 import pytest
 from selenium.webdriver.common.by import By
 
 
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
+
+
 
 
--- test_main_page.py
+++ test_main_page.py
@@ -3,15 +3,17 @@
 import pytest
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
-"""
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)               
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-"""
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # initialize the Page Object, pass an instance of driver and url to the constructor
     page.open()                      # open the page
     page.go_to_login_page()          # use the page method - go to the login page
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
